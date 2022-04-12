30b6036a244e4d9357b0b2c60bf90853bcf6b209
Carlos Aguilera <18aguilerac@gmail.com>
2022-04-11 19:02:28 -0700

    updated frames

--- src/handleEvents.cpp
+++ src/handleEvents.cpp
@@ -26,8 +26,7 @@ void handleEvent(const SDL_Event &event, bool &gameOver, SDL_Renderer* &renderer
       dir = Right;
       updateRenderer(vecTextures, renderer);
     }
-  }
-  else if (state[SDL_SCANCODE_LEFT] && state[SDL_SCANCODE_SPACE] && 
+  }else if (state[SDL_SCANCODE_LEFT] && state[SDL_SCANCODE_SPACE] && 
       event.type == SDL_KEYDOWN) {
     for (int i = 0; i < 48; i++) {
       vecTextures[4].rect->y--;
@@ -46,24 +45,29 @@ void handleEvent(const SDL_Event &event, bool &gameOver, SDL_Renderer* &renderer
       dir = Left;
       updateRenderer(vecTextures, renderer);
     }
-  }
-  else if (event.key.keysym.sym == SDLK_RIGHT && event.type == SDL_KEYDOWN) {
-    if (vecTextures[4].rect->x <= 575)
-      vecTextures[4].rect->x += 12;
+  }else if (event.key.keysym.sym == SDLK_RIGHT && event.type == SDL_KEYDOWN) {
+    if (vecTextures[4].rect->x <= 575) {
+      for (int i = 0; i < 12; i++){
+        vecTextures[4].rect->x++;
+        updateRenderer(vecTextures, renderer);
+      }
+    }
     if (vecTextures[4].rect->x > 575)
       vecTextures[4].rect->x = 575;
     dir = Right;
     updateRenderer(vecTextures, renderer);
-  }
-  else if (event.key.keysym.sym == SDLK_LEFT && event.type == SDL_KEYDOWN) {
-    if (vecTextures[4].rect->x >= 0)
-      vecTextures[4].rect->x -= 12;
+  }else if (event.key.keysym.sym == SDLK_LEFT && event.type == SDL_KEYDOWN) {
+    if (vecTextures[4].rect->x >= 0) {
+      for (int i = 0; i < 12; i++){
+        vecTextures[4].rect->x--;
+        updateRenderer(vecTextures, renderer);
+      }
+    }
     if (vecTextures[4].rect->x < 0)
       vecTextures[4].rect->x = 0;
     dir = Left;
     updateRenderer(vecTextures, renderer);
-  }
-  else if (event.key.keysym.sym == SDLK_SPACE && event.type == SDL_KEYDOWN) {
+  }else if (event.key.keysym.sym == SDLK_SPACE && event.type == SDL_KEYDOWN) {
     for (int i = 0; i < 48; i++) {
       vecTextures[4].rect->y--;
       updateRenderer(vecTextures, renderer);
