56d212560dce8470bac5d0e45db89cece64c01b0
Carlos Aguilera <18aguilerac@gmail.com>
2022-04-23 20:20:25 -0700

    updating

--- src/memory/initSDL.cpp
+++ src/memory/initSDL.cpp
@@ -3,10 +3,13 @@
 bool initSDL(SDL_Window* &window, SDL_Renderer* &renderer) {
   if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == 0) {
     SDL_CreateWindowAndRenderer(640, 480, SDL_RENDERER_ACCELERATED, 
-                                  &window, &renderer);
+                                &window, &renderer);
     std::cout << "SDL initialized\n";
     return true;
   }
   std::cout << "SDL was not initialized!\n";
   return false;
+  // This is a merge conflict
+  // This is a merge conflict
+  // This is a merge conflict
 }
