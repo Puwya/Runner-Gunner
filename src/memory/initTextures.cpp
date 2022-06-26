7e4617f2436f055f47d2a7024f4188106115b7de
Carlos Aguilera <18aguilerac@gmail.com>
2022-06-26 11:57:18 -0700

    Google style

--- src/memory/initTextures.cpp
+++ src/memory/initTextures.cpp
@@ -5,9 +5,9 @@ void initTextures(std::vector<Texture2D> &backgroundTextures) {
   std::string texture_load[4] {"../assets/BackGround.png", "../assets/clouds.png", 
                                "../assets/clouds1.png", "../assets/sky.png"};
   // This loop iterates through the amount of backgroundTextures program needs
-  for (int i = 0; i < 4; i++) {
-    char* cstr = new char [texture_load[i].length()+1];
-    strcpy(cstr, texture_load[i].c_str());
+  for (auto texture : texture_load) {
+    char* cstr = new char [texture.length()+1];
+    strcpy(cstr, texture.c_str());
     backgroundTextures.push_back(LoadTexture(cstr));
     delete []cstr;
   }
